f6dc31ed417e74f8296c8617be74021e868d6567
Bello Olamilekan <belloolamilekan661@gmail.com>
2025-02-18 09:41:54 +0100

    Implemented authentication UI

---
+++ components/auth/card-wrapper.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { Card, CardContent, CardFooter, CardHeader } from "../ui/card";
+import BackButton from "./back-button";
+import Header from "./header";
+import Social from "./social";
+
+interface CardWrapperProps {
+  children: React.ReactNode;
+  headerLabel: string;
+  backButtonLabel: string;
+  backButtonHref: string;
+  showSocials?: boolean;
+}
+
+const CardWrapper = ({
+  children,
+  headerLabel,
+  backButtonHref,
+  backButtonLabel,
+  showSocials,
+}: CardWrapperProps) => {
+  return (
+    <Card className="w-[400px] shadow-md">
+      <CardHeader>
+        <Header label={headerLabel} />
+      </CardHeader>
+      <CardContent>{children}</CardContent>
+      {showSocials && (
+        <CardFooter>
+          <Social />
+        </CardFooter>
+      )}
+      <CardFooter>
+        <BackButton label={backButtonLabel} href={backButtonHref} />
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default CardWrapper;
